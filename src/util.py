bd581d4c25afa41b47966772b0261a8f16dc737f
Ancor Gonzalez <66569444+Coco-os@users.noreply.github.com>
2025-11-05 11:07:42 +0000

    Added split

--- src/util.py
+++ src/util.py
@@ -16,3 +16,40 @@ def create_mini_batches(
         end_idx = min(start_idx + batch_size, num_samples)
         batch_indices = indices[start_idx:end_idx]
         yield X[batch_indices], y[batch_indices]
+
+
+def train_val_test_split(
+    X: np.ndarray,
+    y: np.ndarray,
+    train_size: float = 0.7,
+    val_size: float = 0.15,
+    test_size: float = 0.15,
+    random_seed: int | None = None,
+) -> Tuple[
+    np.ndarray, np.ndarray, np.ndarray, np.ndarray, np.ndarray, np.ndarray
+]:
+
+    assert (
+        train_size + val_size + test_size == 1.0
+    ), "Las proporciones deben sumar 1.0"
+
+    num_samples = X.shape[0]
+    indices = np.arange(num_samples)
+
+    if random_seed is not None:
+        np.random.seed(random_seed)
+
+    np.random.shuffle(indices)
+
+    train_end = int(train_size * num_samples)
+    val_end = train_end + int(val_size * num_samples)
+
+    train_idx = indices[:train_end]
+    val_idx = indices[train_end:val_end]
+    test_idx = indices[val_end:]
+
+    X_train, y_train = X[train_idx], y[train_idx]
+    X_val, y_val = X[val_idx], y[val_idx]
+    X_test, y_test = X[test_idx], y[test_idx]
+
+    return X_train, y_train, X_val, y_val, X_test, y_test
