59e0a543a40c5c187606847241c2af154fa9dc5e
giselabcruz <giselaisabelbelmontecruz@gmail.com>
2025-11-06 02:01:12 +0000

    update neural_network.py: methods for test, testing loss, accuracy and create the neural network calling the fully connected layer

--- src/neural_network/neural_network.py
+++ src/neural_network/neural_network.py
@@ -48,3 +48,73 @@ def train(network, loss, x_train, y_train, x_val=None, y_val=None, epochs=100):
     return history
 
 
+def test_nn(nn, x_test, y_test, loss=None):
+    correct_predictions = 0
+    total_predictions = len(x_test)
+
+    y_pred = []
+    y_true = []
+
+    for x, y in zip(x_test, y_test):
+        output = predict(nn, x)
+        predicted_class = np.argmax(output)
+        true_class = np.argmax(y)
+        y_true.append(true_class)
+        y_pred.append(predicted_class)
+
+        if predicted_class == true_class:
+            correct_predictions += 1
+
+    accuracy = correct_predictions / total_predictions
+
+    loss_message = ""
+
+    if loss is not None:
+        error = test_loss(nn, loss, x_test, y_test)
+        loss_message = f'Loss: {error}, '
+    print(f'{loss_message}Accuracy: {round(accuracy * 100, 2)}%')
+    return y_true, y_pred
+
+
+def test_loss(nn, loss, x_test, y_test):
+    error = 0
+    for x, y in zip(x_test, y_test):
+        output = predict(nn, x)
+
+        error += loss(y, output)
+    error /= len(x_test)
+    print(f"Loss in test = {error}")
+    return error
+
+
+def validation_accuracy(nn, x_val, y_val):
+    correct_predictions = 0
+    total_predictions = len(x_val)
+
+    for x, y in zip(x_val, y_val):
+        output = predict(nn, x)
+        predicted_class = np.argmax(output)
+        true_class = np.argmax(y)
+
+        if predicted_class == true_class:
+            correct_predictions += 1
+
+    return correct_predictions / total_predictions
+
+
+def clip_gradient(grad, threshold=3.0):
+    norm = np.linalg.norm(grad)
+    if norm > threshold:
+        grad = grad * (threshold / norm)
+    return grad
+
+
+def create_nn(neurons_layer, activation_function, optimizers):
+    nn = []
+    for i in range(len(neurons_layer) - 1):
+        nn.append(FullyConnectedLayer(neurons_layer[i], neurons_layer[i + 1], optimizers[i]))
+        if i < len(activation_function):
+            nn.append(activation_function[i])
+
+    initialize_optimizer(nn, optimizers)
+    return nn
