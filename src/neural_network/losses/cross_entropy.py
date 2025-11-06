fbc5a06cdc7b17825a163418928ab027f36e59f6
giselabcruz <giselaisabelbelmontecruz@gmail.com>
2025-11-06 02:17:29 +0000

    update losses: cross entropy loss

---
+++ src/neural_network/losses/cross_entropy.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+class CrossEntropyLoss:
+
+    def __init__(self):
+        pass
+
+    def __call__(self, y_true, y_pred):
+        epsilon = 1e-15
+        y_pred = np.clip(y_pred, epsilon, 1 - epsilon)
+        return -np.sum(y_true * np.log(y_pred))
+
+    @staticmethod
+    def derivative(y_true, y_pred):
+        return y_pred - y_true
